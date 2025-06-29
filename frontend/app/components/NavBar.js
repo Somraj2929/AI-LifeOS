cb0fdfbcd7513a47aa0b324ea3c6cac50caf5c43
Somraj <somrajbishnoirock29@gmail.com>
2025-06-30 04:16:49 +0530

    Refactor Navbar component to use Link for brand navigation and fix dependency name in package.json

--- frontend/app/components/NavBar.js
+++ frontend/app/components/NavBar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Bars4Icon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -10,8 +11,9 @@ export default function Navbar() {
         <Bars4Icon className="h-6 w-6 text-black" />
       </button>
 
-      {/* Brand */}
-      <h1 className="text-[28px] font-bold text-black">AI LifeOS</h1>
+      <Link href="/">
+        <h1 className="text-[28px] font-bold text-black">AI LifeOS</h1>
+      </Link>
 
       {/* Profile Icon */}
       <Image
